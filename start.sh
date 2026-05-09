143ec2eec21aa619e833a1d2c43689b2939dc166
wataruian <wiimorata@gmail.com>
2026-05-09 11:41:10 +0800

    chore: update viteplus

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-vp run serve
+mode="${1:-dev}"
+
+if [[ "${mode}" != "dev" && "${mode}" != "prod" ]]; then
+  echo "Invalid mode: ${mode}"
+  exit 1
+fi
+
+echo "Mode: ${mode}"
+vp run "serve:${mode}"
