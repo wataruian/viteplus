6d68275312e2bd48507e98f01ff29a55855459a4
wataruian <wiimorata@gmail.com>
2026-08-05 00:38:42 +0800

    chore: update viteplus

--- clean.sh
+++ clean.sh
@@ -4,11 +4,11 @@ tools="${1:-"false"}"
 ai="${2:-"false"}"
 
 if [[ "${tools}" == "true" ]]; then
-  vp run clean:tools
+  vp run --no-cache clean:tools
 fi
 
 if [[ "${ai}" == "true" ]]; then
-  vp run clean:ai
+  vp run --no-cache clean:ai
 fi
 
-vp run clean:build
+vp run --no-cache clean:build
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-vp run init
+vp run --no-cache init
